78183122d3699f3330c4b1b6a306a798a26ae508
ZhangJin <nbnbnb.zhang@gmail.com>
2016-06-11 21:32:34 +0800

    C# with Python

---
+++ ConsoleAppWithPython/Program.cs
@@ -0,0 +1,23 @@
+﻿using IronPython.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 安装 Python 库
+
+            //Install - Package IronPython
+            //Install - Package IronPython.StdLib
+
+            var engine = Python.CreateEngine();
+            engine.CreateScriptSourceFromString("print('Hello IronPython!')").Execute();
+        }
+    }
+}
