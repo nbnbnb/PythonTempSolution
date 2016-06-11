6cdd844973e5dc1e0af948a14a4909f93a366a01
ZhangJin <nbnbnb.zhang@gmail.com>
2016-06-11 20:32:25 +0800

    Add IronPython Demos

---
+++ IronPythonFormApp/TemplateMainForm.py
@@ -0,0 +1,45 @@
+﻿import clr
+clr.AddReference('System.Drawing')
+clr.AddReference('System.Windows.Forms')
+
+# 把 WindowsFormApp 的生成输出目录设置到当前文件夹中
+# 一定要将源代码格式设置为 UTF8+BOM，否则无法输入中文
+
+# 需要将控件的 Motify 设置为 Pubulic
+# 这样才能在 Python 中进行访问
+clr.AddReferenceToFile('WindowsFormApp.exe') 
+#clr.AddReference('WindowsFormApp')
+
+from System import *
+from System.Drawing import *
+from System.Windows.Forms import *
+from WindowsFormApp import MainForm
+
+
+class TemplateMainForm(MainForm): 
+    def __init__(self):
+        # Create child controls and initialize form
+        # self.buttonAddToList.Click+= self.addTime
+        self.buttonAddToList.Click+= lambda sender,event : self.listBoxTimes.Items.Add(DateTime.Now.ToString())
+        self.listBoxTimes.Click+=self.showItem
+    def addTime(self,target,event):
+        self.listBoxTimes.Items.Add(DateTime.Now.ToString())
+        self.log('addTime')
+
+    def showItem(self,target,event):
+        if self.listBoxTimes.SelectedItem:
+            MessageBox.Show(self.listBoxTimes.SelectedItem.ToString())
+            self.log('showItem')
+
+    def log(self,message):
+        # TODO：两种方式在 VS 调试器中都无法输出
+        Console.WriteLine(message)
+        print(message)
+
+
+
+Application.EnableVisualStyles()
+Application.SetCompatibleTextRenderingDefault(False)
+
+form = TemplateMainForm()
+Application.Run(form)
---
+++ WindowsFormApp/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAddToList = new System.Windows.Forms.Button();
+            this.listBoxTimes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonAddToList
+            // 
+            this.buttonAddToList.Location = new System.Drawing.Point(34, 40);
+            this.buttonAddToList.Name = "buttonAddToList";
+            this.buttonAddToList.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddToList.TabIndex = 0;
+            this.buttonAddToList.Text = "AddToList";
+            this.buttonAddToList.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTimes
+            // 
+            this.listBoxTimes.FormattingEnabled = true;
+            this.listBoxTimes.ItemHeight = 12;
+            this.listBoxTimes.Location = new System.Drawing.Point(149, 40);
+            this.listBoxTimes.Name = "listBoxTimes";
+            this.listBoxTimes.Size = new System.Drawing.Size(321, 280);
+            this.listBoxTimes.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 353);
+            this.Controls.Add(this.listBoxTimes);
+            this.Controls.Add(this.buttonAddToList);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonAddToList;
+        public System.Windows.Forms.ListBox listBoxTimes;
+    }
+}
+
