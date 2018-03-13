4bc8af64c7b0996771e28c451ce5ebbf79f91764
张进 <jinzhanga@Ctrip.com>
2018-03-13 16:38:34 +0800

    cwd

--- ConsoleApp/ConsoleApp.py
+++ ConsoleApp/ConsoleApp.py
@@ -2,6 +2,16 @@
 print('-------------- start-------------')
 print()
 
+import gettext
+
+t = gettext.translation('example', './locale', fallback=True)
+_ = t.gettext
+print(_('This message is in the script.'))
+
+with open('abc.txt') as f:
+    print(f.read())
+
+
 if __name__ == '__main__':
     print()
     print('--------------- end -------------')
